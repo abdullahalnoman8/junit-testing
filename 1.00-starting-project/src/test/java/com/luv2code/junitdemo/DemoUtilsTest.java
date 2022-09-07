a0ec37d11c8d202fb8a8e6a41910f7ee1c70ecb1
Abdullah Al Noman <abdullah.noman@advisori.de>
2022-09-07 22:59:40 +0200

    Test Lifecycle Methods

--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -1,21 +1,47 @@
 package com.luv2code.junitdemo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DemoUtilsTest {
 
+    DemoUtils demoUtils;
+
+    @BeforeEach
+    void setupBeforeEach(){
+        demoUtils = new DemoUtils();
+        System.out.println("BeforeEach executes before the execution of each test method");
+    }
+    @AfterEach
+    void tearDownAfterEach(){
+        System.out.println("Running @AfterEach");
+        System.out.println();
+    }
+    @BeforeAll
+    static void setupBeforeEachClass(){
+        System.out.println("@BeforeAll executes only once before all test methods execution in the class");
+    }
+
+    @AfterAll
+    static void tearDownAfterAll(){
+        System.out.println("@AfterAll executes only once after all test method execution in the class");
+    }
     @Test
     void testEqualsAndNotEquals(){
-        DemoUtils demoUtils = new DemoUtils();
+
+        System.out.println("Running test: testEqualsAndNotEquals");
+
+        //DemoUtils demoUtils = new DemoUtils();
         assertEquals(6, demoUtils.add(2,4),"2 + 4 must be 6");
         assertNotEquals(8,demoUtils.add(6,1)," 6 + 1 must not be 8");
     }
 
     @Test
     void testNullAndNotNull(){
-        DemoUtils demoUtils = new DemoUtils();
+
+        System.out.println("Running test: testEqualsAndNotEquals");
+       // DemoUtils demoUtils = new DemoUtils();
         String str1 = null;
         String str2 = "Luv2Code";
 
